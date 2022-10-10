62239b9b5a370ba5b583b79a0a95945d7c6a265f
Silveth <silveth.chavero@hotmail.com>
2022-10-10 13:01:35 -0500

    Creacion de componentes Header y Listado de pacientes

---
+++ src/componentes/header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <h1> Hola mundo Silve</h1>
+    )
+    
+    
+}
+
+
+export default Header
---
+++ src/componentes/listadopacientes.jsx
@@ -0,0 +1,9 @@
+function ListadoPacientes(){
+    return(
+        <h2> Estes es el listado</h2>
+    )
+    
+    
+}
+
+export default ListadoPacientes
