a08f71bd0d9e602e5949dffd55201f0555903987
Silveth <silveth.chavero@hotmail.com>
2022-10-13 17:25:01 -0500

    formulario
    Se incorporan estilos, al formulario

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,9 +1,8 @@
-function Header(){
-    return(
-        <h1> Hola mundo</h1>
-    )
-    
-    
+function Header() {
+    //Cada componente debe tener un return
+    //Debe estar en el nivel mas alto y solo 1
+
+    return (<h1 className="font-black text-center mx-auto text-4xl">Seguiemiento de pacientes <span className="text-indigo-400"> Veterinaria</span></h1>)
 }
 
 export default Header
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+
+  content: ["index.html", "./src/**/*.jsx"],
+
+  theme: {
+
+    extend: {},
+
+  },
+
+  plugins: [],
+
+}
